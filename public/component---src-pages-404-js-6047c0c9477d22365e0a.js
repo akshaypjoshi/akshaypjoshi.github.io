5f7d9478bb0fa474d48b2a316f7f24ae2cf1d77d
Akshay Joshi <akshaypjoshi@gmail.com>
2019-09-29 01:52:02 +0530

    ### gitignore updated.

--- public/component---src-pages-404-js-6047c0c9477d22365e0a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{211:function(e,t,a){"use strict";a.r(t);var n=a(0),l=a.n(n),s=a(205);t.default=function(){return l.a.createElement(s.a,null,l.a.createElement("div",{id:"main",className:"alt"},l.a.createElement("section",{id:"one"},l.a.createElement("div",{className:"inner"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-6047c0c9477d22365e0a.js.map
