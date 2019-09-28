c89c12bb5341f0325df0a7e5b770ba224d83aa6c
Akshay Joshi <akshaypjoshi@gmail.com>
2019-09-29 02:47:42 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# akshaypjoshi.github.io
---
+++ component---src-pages-404-js-b15ed9141d67e827357b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{212:function(e,t,a){"use strict";a.r(t);var n=a(0),l=a.n(n),s=a(206);t.default=function(){return l.a.createElement(s.a,null,l.a.createElement("div",{id:"main",className:"alt"},l.a.createElement("section",{id:"one"},l.a.createElement("div",{className:"inner"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b15ed9141d67e827357b.js.map
